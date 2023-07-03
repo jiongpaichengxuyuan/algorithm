58953e9614c948f5cb308cd3988c796113dac6c0
jiongpaichengxuyuan <570073523@qq.com>
2023-07-03 15:47:04 +0800

    print binarytree iteratively

--- tests/construct_binary_tree.cc
+++ tests/construct_binary_tree.cc
@@ -7,7 +7,18 @@
 using namespace std;
 
 int main() {
-  vector<int> vec = {4, 1, 6, 0, 2, 5, 7, -1, -1, -1, 3, -1, -1, -1, 9};
+  vector<int> vec = {4, 1, 6, 0, 2, 5, 7, -1, -1, -1, 3, -1, -1, -1, 8};
   BinaryTreeNode* root = ConstructBinaryTree(vec, 0);
-  PrintBinaryTreeLevel(root);
+  PrintBinaryTreePre(root);
+  std::cout << std::endl;
+  PrintBinaryTreePreIteratively(root);
+  std::cout << std::endl;
+  PrintBinaryTreeIn(root);
+  std::cout << std::endl;
+  PrintBinaryTreeInIteratively(root);
+  std::cout << std::endl;
+  PrintBinaryTreePost(root);
+  std::cout << std::endl;
+  PrintBinaryTreePostIteratively(root);
+  std::cout << std::endl;
 }
--- utils/data_structure.h
+++ utils/data_structure.h
@@ -1,8 +1,11 @@
 #ifndef ALGORITHM_UTILS_DATA_STRUCTURE_H_
 #define ALGORITHM_UTILS_DATA_STRUCTURE_H_
 
+#include <algorithm>
 #include <iostream>
 #include <queue>
+#include <stack>
+#include <vector>
 
 #pragma region BinaryTree
 
@@ -35,17 +38,69 @@ void PrintBinaryTreePre(BinaryTreeNode *root) {
 // 中序遍历
 void PrintBinaryTreeIn(BinaryTreeNode *root) {
   if (root == nullptr) return;
-  PrintBinaryTreePre(root->left);
+  PrintBinaryTreeIn(root->left);
   std::cout << root->val << " ";
-  PrintBinaryTreePre(root->right);
+  PrintBinaryTreeIn(root->right);
 }
 
 // 后序遍历
 void PrintBinaryTreePost(BinaryTreeNode *root) {
   if (root == nullptr) return;
-  PrintBinaryTreePre(root->left);
-  PrintBinaryTreePre(root->right);
+  PrintBinaryTreePost(root->left);
+  PrintBinaryTreePost(root->right);
   std::cout << root->val << " ";
+}
+
+// 迭代前序遍历
+// 思路：每一次都把当前节点弹出，然后将其右、左节点压入
+void PrintBinaryTreePreIteratively(BinaryTreeNode *root) {
+  if (root == nullptr) return;
+  std::stack<BinaryTreeNode *> node_stack;
+  node_stack.push(root);
+  while (!node_stack.empty()) {
+    auto node = node_stack.top();
+    node_stack.pop();
+    std::cout << node->val << " ";
+    if (node->right) node_stack.push(node->right);
+    if (node->left) node_stack.push(node->left);
+  }
+}
+
+// 迭代中序遍历
+// 思路：
+// 利用cur_node指针遍历二叉树，利用栈处理节点，只要当前cur_node是nullptr，那么就可以进行处理，即当前栈顶的节点需要被处理
+void PrintBinaryTreeInIteratively(BinaryTreeNode *root) {
+  if (root == nullptr) return;
+  std::stack<BinaryTreeNode *> node_stack;
+  auto cur_node = root;
+  while (cur_node || !node_stack.empty()) {
+    if (cur_node) {
+      node_stack.push(cur_node);
+      cur_node = cur_node->left;
+    } else {
+      cur_node = node_stack.top();
+      node_stack.pop();
+      std::cout << cur_node->val << " ";
+      cur_node = cur_node->right;
+    }
+  }
+}
+
+// 迭代后序遍历
+void PrintBinaryTreePostIteratively(BinaryTreeNode *root) {
+  if (root == nullptr) return;
+  std::stack<BinaryTreeNode *> node_stack;
+  node_stack.push(root);
+  std::vector<int> result;
+  while (!node_stack.empty()) {
+    auto node = node_stack.top();
+    node_stack.pop();
+    result.push_back(node->val);
+    if (node->left) node_stack.push(node->left);
+    if (node->right) node_stack.push(node->right);
+  }
+  reverse(result.begin(), result.end());
+  for (auto i : result) std::cout << i << " ";
 }
 
 // 层序遍历 or 广度优先遍历
